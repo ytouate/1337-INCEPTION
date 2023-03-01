73241e8092533552e2f24a60e0ec85a84b8b435c
ytouate <touateyoussef2003@gmail.com>
2023-03-01 21:18:31 +0100

    modifing the named volume of mariadb

--- srcs/requirements/mariadb/tools/setup.sh
+++ srcs/requirements/mariadb/tools/setup.sh
@@ -7,6 +7,8 @@ mariadb -u root -e "CREATE USER 'ytouate'@'%' IDENTIFIED BY 'pass';"
 mariadb -u root -e "GRANT ALL PRIVILEGES ON wp_db.* TO 'ytouate'@'%';"
 mariadb -u root -e "FLUSH PRIVILEGES;"
 
+sleep 2
+
 /etc/init.d/mariadb stop
 
 exec $@
