c2370e3e28afa52933bd0610c346bcf7c0fe2a5f
ytouate <touateyoussef2003@gmail.com>
2023-03-02 18:25:32 +0100

    checking if the user already exits before creating it

--- srcs/requirements/wordpress/tools/setup.sh
+++ srcs/requirements/wordpress/tools/setup.sh
@@ -4,7 +4,10 @@ sleep 3
 cd /home/ytouate/data/wordpress && \
 wp core install --url=$WORDPRESS_DOMAIN_NAME --title=$WORDPRESS_DB_USER --admin_user=$WORDPRESS_DB_USER \
 --admin_password=$WORDPRESS_DB_PASSWORD --admin_email=$WORDPRESS_USER_EMAIL --skip-email && \
-wp user create ilefhail ilefhail@gmail.com --user_pass=$WORDPRESS_DB_PASSWORD && \
+if  [ -z "`wp user list | grep ilefhail`" ]
+then
+    wp user create ilefhail ilefhail@gmail.com --user_pass="$WORDPRESS_DB_PASSWORD"
+fi
 cd /etc/php81/php-fpm.d  && \
 sed -i 's/listen = 127.0.0.1:9000/listen = wordpress:9000/g' www.conf && \
 cd /
